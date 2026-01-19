355ceb0fc5e1f52da0100310054b7fb4c23f6404
Aleksandar Samardžić <asamardzic@openteams.com>
2026-01-19 20:13:39 +0000

    Add Gluon based grouped MM kernel for Blackwell into Inductor
    ghstack-source-id: cf27c49fc56a88d39eb61caf188ca0604f25fe78
    Pull-Request: https://github.com/pytorch/pytorch/pull/166063

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -5943,6 +5943,7 @@ class TritonScheduling(SIMDScheduling):
             BackendFeature.SCAN,
             BackendFeature.SORT,
             BackendFeature.TRITON_TEMPLATES,
+            BackendFeature.GLUON_TEMPLATES,
             BackendFeature.TUPLE_REDUCTION,
         ]
     )
